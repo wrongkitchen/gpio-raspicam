cfb341ed2f18c942e15bc3b6d93675873d17ec72
wrongkitchen <wkc.kenji@gmail.com>
2014-08-17 05:28:23 +0000

    Shaun home config

--- python/gpio-camera.py
+++ python/gpio-camera.py
@@ -10,6 +10,6 @@ GPIO.setup(18, GPIO.IN, GPIO.PUD_UP)
 with picamera.PiCamera() as camera:
 	camera.start_preview()
 	GPIO.wait_for_edge(18, GPIO.FALLING)
-	camera.capture('/home/pi/project/py-gpio/photo/image.jpg')
-	os.system('scp /home/pi/project/py-gpio/photo/image.jpg admin@wrongkitchen.mycloudnas.com:/share/HDA_DATA/Qweb/scp2/cam1_123.jpg')
+	camera.capture('/home/pi/project/gpio-raspicam/photo/image.jpg')
 	camera.stop_preview()
+	os.system('scp /home/pi/project/gpio-raspicam/photo/image.jpg pi@shaun-mbbc:~/Desktop/pi/cam1.jpg')
